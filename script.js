f9a68a4b1239b8a5dbcbc4edece0dd8231b9698b
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-05 21:54:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	  if (!str) return false;
+
+  const regex = /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,3}(\.[a-zA-Z]{2,3})?$/;
+
+  return regex.test(str);
 }
 
 // Do not change the code below.
